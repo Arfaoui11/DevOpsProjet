58cf6154884cd0cb49637816e35af24698cf2442
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-09-19 21:33:16 +0100

    new

---
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
@@ -0,0 +1,26 @@
+package com.esprit.examen.services;
+
+import static org.junit.Assert.*;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.esprit.examen.entities.Stock;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class ProduitServiceImplTest {
+    @Autowired
+    IProduitService produitService;
+
+
+
+
+
+
+
+
+}
