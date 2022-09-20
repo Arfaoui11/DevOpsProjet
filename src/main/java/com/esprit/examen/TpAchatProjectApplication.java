2a9b446614a169bf410af3b2d175509dba5d2f97
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-09-20 02:31:58 +0100

    new

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -4,9 +4,11 @@ package com.esprit.examen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @EnableScheduling
+@EnableSwagger2
 @SpringBootApplication
 public class TpAchatProjectApplication {
 
