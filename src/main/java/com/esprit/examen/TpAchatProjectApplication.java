35f85b65ff6258f378c31447fa5c682fd27fa383
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-09-20 02:48:09 +0100

    new

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -4,15 +4,23 @@ package com.esprit.examen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @EnableScheduling
 @EnableSwagger2
 @SpringBootApplication
+@RestController
 public class TpAchatProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpAchatProjectApplication.class, args);
 }
+
+	@GetMapping("/")
+	public String home(){
+		return "Welcome Home";
+	}
 }
