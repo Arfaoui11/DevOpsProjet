81732aafda6d90b22caf915e17636392c6957e15
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-10-07 21:15:58 +0100

    nn

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -21,6 +21,6 @@ public class TpAchatProjectApplication {
 
 	@GetMapping("/")
 	public String home(){
-		return "Welcome Home new mahdi change 2022";
+		return "Welcome Home new mahdi change 2022 tt";
 	}
 }
