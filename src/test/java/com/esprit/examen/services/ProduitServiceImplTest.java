f00ecff03fb3dbc4dbaa12c9d1953b50914d8286
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-10-27 22:06:32 +0100

    new update mahdi arfaoui

--- src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
@@ -12,16 +12,15 @@ import com.esprit.examen.entities.Produit;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import com.esprit.examen.entities.Stock;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProduitServiceImplTest {
     @Autowired
     IProduitService produitService;
