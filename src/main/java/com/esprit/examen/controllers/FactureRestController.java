152fdae0ceca43c1c933f91c80651aa49e17468f
chaimayahyaoui123 <57320951+chaimayahyaoui123@users.noreply.github.com>
2022-10-28 13:28:45 +0200

    Merge remote-tracking branch 'origin/chaima_back' into chaima_back

--- src/main/java/com/esprit/examen/controllers/FactureRestController.java
+++ src/main/java/com/esprit/examen/controllers/FactureRestController.java
@@ -25,8 +25,7 @@ public class FactureRestController {
     @GetMapping("/retrieve-all-factures")
     @ResponseBody
     public List<Facture> getFactures() {
-        List<Facture> list = factureService.retrieveAllFactures();
-        return list;
+        return factureService.retrieveAllFactures();
     }
 
     // http://localhost:8089/SpringMVC/facture/retrieve-facture/8
