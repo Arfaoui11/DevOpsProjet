5a997bfc1cbe4893aeb57be771f7c3745b6bc8f8
chaimayahyaoui123 <57320951+chaimayahyaoui123@users.noreply.github.com>
2022-10-28 18:07:59 +0200

    Merge remote-tracking branch 'origin/chaima_back' into chaima_back

--- src/main/java/com/esprit/examen/controllers/FactureRestController.java
+++ src/main/java/com/esprit/examen/controllers/FactureRestController.java
@@ -18,42 +18,33 @@ import io.swagger.annotations.Api;
 @CrossOrigin("*")
 public class FactureRestController {
 
+
     @Autowired
     IFactureService factureService;
 
 
-    /*
-     * http://localhost:8089/SpringMVC/facture/retrieve-all-factures
-     */
+
     @GetMapping("/retrieve-all-factures")
     @ResponseBody
     public List<Facture> getFactures() {
         return factureService.retrieveAllFactures();
     }
-    /*
-      * // http://localhost:8089/SpringMVC/facture/retrieve-facture/8
 
-     */
        @GetMapping("/retrieve-facture/{facture-id}")
        @ResponseBody
        public Facture retrieveFacture(@PathVariable("facture-id") Long factureId) {
            return factureService.retrieveFacture(factureId);
        }
 
-       /*
-        * http://localhost:8089/SpringMVC/facture/add-facture/{fournisseur-id}
-        */
+
     @PostMapping("/add-facture")
     @ResponseBody
     public Facture addFacture(@RequestBody Facture f) {
-        Facture facture = factureService.addFacture(f);
-        return facture;
+
+        return  factureService.addFacture(f);
     }
 
-    /*
-     * une facture peut etre annulé si elle a été saisie par erreur Pour ce
-     * faire, il suffit de mettre le champs active à false
-     */
+
     // http://localhost:8089/SpringMVC/facture/cancel-facture/{facture-id}
     @PutMapping("/cancel-facture/{facture-id}")
     @ResponseBody
@@ -61,27 +52,18 @@ public class FactureRestController {
         factureService.cancelFacture(factureId);
     }
 
-     /*
-       * http://localhost:8089/SpringMVC/facture/getFactureByFournisseur/{fournisseur-id}
-      */
+
     @GetMapping("/getFactureByFournisseur/{fournisseur-id}")
     @ResponseBody
     public List<Facture> getFactureByFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
         return factureService.getFacturesByFournisseur(fournisseurId);
     }
- /*
-  *  // http://localhost:8089/SpringMVC/facture/assignOperateurToFacture/1/1
 
-  */
     @PutMapping(value = "/assignOperateurToFacture/{idOperateur}/{idFacture}")
     public void assignOperateurToFacture(@PathVariable("idOperateur") Long idOperateur, @PathVariable("idFacture") Long idFacture) {
         factureService.assignOperateurToFacture(idOperateur, idFacture);
     }
 
-     /*
-     * http://localhost:8089/SpringMVC/facture/pourcentageRecouvrement/{startDate}/{endDate}
-
-      */
 
     @GetMapping(value = "/pourcentageRecouvrement/{startDate}/{endDate}")
     public float pourcentageRecouvrement(
