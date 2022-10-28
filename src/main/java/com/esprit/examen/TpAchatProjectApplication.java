1acef4fb27a64283dd49e21201bace165f28ca34
chaimayahyaoui123 <57320951+chaimayahyaoui123@users.noreply.github.com>
2022-10-28 13:28:45 +0200

    Merge remote-tracking branch 'origin/chaima_back' into chaima_back

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -3,10 +3,12 @@ package com.esprit.examen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.mappers.ModelMapper;
 
 
 @EnableScheduling
@@ -14,6 +16,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @RestController
 public class TpAchatProjectApplication {
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpAchatProjectApplication.class, args);
