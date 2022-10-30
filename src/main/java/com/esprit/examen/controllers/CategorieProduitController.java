4b2225aab9cc28c3dc35174815361894310b0e60
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-10-30 18:18:23 +0100

    new update mahdi arfaoui

--- src/main/java/com/esprit/examen/controllers/CategorieProduitController.java
+++ src/main/java/com/esprit/examen/controllers/CategorieProduitController.java
@@ -3,15 +3,7 @@ package com.esprit.examen.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.esprit.examen.entities.CategorieProduit;
 import com.esprit.examen.services.ICategorieProduitService;
 import io.swagger.annotations.Api;
@@ -19,6 +11,7 @@ import io.swagger.annotations.Api;
 @RestController
 @Api(tags = "Gestion des categories Produit")
 @RequestMapping("/categorieProduit")
+@CrossOrigin("*")
 public class CategorieProduitController {
 
 	@Autowired
--- src/main/java/com/esprit/examen/controllers/FournisseurRestController.java
+++ src/main/java/com/esprit/examen/controllers/FournisseurRestController.java
@@ -2,15 +2,7 @@ package com.esprit.examen.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.esprit.examen.entities.Fournisseur;
 import com.esprit.examen.services.IFournisseurService;
 
@@ -20,6 +12,7 @@ import io.swagger.annotations.Api;
 @RestController
 @Api(tags = "Gestion des fournisseurss")
 @RequestMapping("/fournisseur")
+@CrossOrigin("*")
 public class FournisseurRestController {
 
 	@Autowired
