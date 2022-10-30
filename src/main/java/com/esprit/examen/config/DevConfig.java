46f3d44e570fad5537229fb810283c089ad32acc
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-10-30 20:14:19 +0100

    new update mahdi arfaoui

--- src/main/java/com/esprit/examen/config/DevConfig.java
+++ src/main/java/com/esprit/examen/config/DevConfig.java
@@ -1,24 +1,19 @@
 package com.esprit.examen.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @Configuration
-@Profile("dev")
-public class DevConfig {
+@EnableWebMvc
+public class DevConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://10.0.0.10:4200");
     }
-
 }
