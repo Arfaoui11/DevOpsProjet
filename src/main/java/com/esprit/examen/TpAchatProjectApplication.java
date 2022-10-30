d030b8f40488a6715e72459f70ac9c9da4bea772
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-10-30 15:08:14 +0100

    new update mahdi arfaoui

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.mappers.ModelMapper;
 
 
 @EnableScheduling
