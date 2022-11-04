97c478dd1276268df6cc4305a8180b0dfae427e7
DrissiOmar <omar.drissi@esprit.tn>
2022-11-05 00:17:20 +0100

    F

--- src/main/java/com/esprit/examen/controllers/OperateurController.java
+++ src/main/java/com/esprit/examen/controllers/OperateurController.java
@@ -14,6 +14,7 @@ import io.swagger.annotations.Api;
 @RestController
 @Api(tags = "Gestion des opérateurs")
 @RequestMapping("/operateur")
+@SuppressWarnings("restriction")
 @CrossOrigin("*")
 public class OperateurController {
 
