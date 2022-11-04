70e795fbbb6642c26addbb51a435938e66f6019c
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-11-04 23:18:28 +0100

    new update mahdi arfaoui

--- src/test/java/com/esprit/examen/services/ProduitServiceImplMockTest.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplMockTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.*;
 
 
-@SpringBootTest(classes = ProduitServiceImplMockTest.class)
+
 @ExtendWith(MockitoExtension.class)
 public class ProduitServiceImplMockTest {
 
--- src/test/java/com/esprit/examen/services/StockServiceImplMockTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplMockTest.java
@@ -15,7 +15,7 @@ import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.*;
 
 
-@SpringBootTest(classes = StockServiceImplMockTest.class)
+
 @ExtendWith(MockitoExtension.class)
 public class StockServiceImplMockTest {
 
