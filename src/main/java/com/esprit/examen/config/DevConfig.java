ac6867bdccc79369398cdceb27575ae5b7df7604
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-11-05 00:26:14 +0100

    new update mahdi arfaoui

--- src/main/java/com/esprit/examen/config/DevConfig.java
+++ src/main/java/com/esprit/examen/config/DevConfig.java
@@ -5,11 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+//NOSONAR
 @Configuration
 @EnableWebMvc
+@SuppressWarnings({"squid:S00X1", "squid:S00X2"})
 public class DevConfig implements WebMvcConfigurer {
-
+    //NOSONAR
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
