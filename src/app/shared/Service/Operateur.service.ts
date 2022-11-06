3510f23b0e812bbb37c196ce1c30defcd71763d0
DrissiOmar <omar.drissi@esprit.tn>
2022-11-06 20:13:34 +0100

    update ip

--- src/app/shared/Service/Operateur.service.ts
+++ src/app/shared/Service/Operateur.service.ts
@@ -5,20 +5,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OperateurService {
-  readonly API_URL = 'http://192.168.1.27:8089/SpringMVC/operateur';
+  //readonly API_URL = 'http://192.168.1.27:8089/SpringMVC/operateur';
 
   constructor(private httpClient: HttpClient) { }
 
   getAllOperateurs() {
-    return this.httpClient.get(`${this.API_URL}/retrieve-all-operateurs`)
+    return this.httpClient.get(`http://192.168.1.27:8089/SpringMVC/operateur/retrieve-all-operateurs`)
   }
   addOperateur(operateur : any) {
-    return this.httpClient.post(`${this.API_URL}/add-operateur`, operateur)
+    return this.httpClient.post(`http://192.168.1.27:8089/SpringMVC/operateur/add-operateur`, operateur)
   }
   editOperateur(operateur : any){
-    return this.httpClient.put(`${this.API_URL}/modify-operateur`, operateur)
+    return this.httpClient.put(`http://192.168.1.27:8089/SpringMVC/operateur/modify-operateur`, operateur)
   }
   deleteOperateur(idOperateur : any){
-    return  this.httpClient.delete(`${this.API_URL}/remove-operateur/${idOperateur}`)
+    return  this.httpClient.delete(`http://192.168.1.27:8089/SpringMVC/operateur/remove-operateur/${idOperateur}`)
   }
 }
