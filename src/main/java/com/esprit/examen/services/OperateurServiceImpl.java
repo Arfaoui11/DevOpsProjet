c05118da6de477f47be80e01b75997778ff3c2e6
DrissiOmar <omar.drissi@esprit.tn>
2022-11-06 01:04:13 +0100

    fix

--- src/main/java/com/esprit/examen/services/OperateurServiceImpl.java
+++ src/main/java/com/esprit/examen/services/OperateurServiceImpl.java
@@ -3,7 +3,6 @@ package com.esprit.examen.services;
 import java.util.Date;
 import java.util.List;
 
-import com.esprit.examen.entities.Produit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,42 +17,30 @@ public class OperateurServiceImpl implements IOperateurService {
 	OperateurRepository  operateurRepository;
 	@Override
 	public List<Operateur> retrieveAllOperateurs() {
-		log.info("In method retrieveAllOperateurs");
-		List<Operateur> operateurs = (List<Operateur>) operateurRepository.findAll();
-		for (Operateur op : operateurs) {
-			log.info(" Operateur: " + op);
-		}
-		log.info(" out of retrieveAllOperateurs");
-		return operateurs;
+		return (List<Operateur>) operateurRepository.findAll();
 	}
 
 	@Override
 	public Operateur addOperateur(Operateur op) {
-		log.info("In method addOperateur");
 		operateurRepository.save(op);
 		return op;
 	}
 
 	@Override
 	public void deleteOperateur(Long id) {
-		log.info("In method deleteOperateur");
 		operateurRepository.deleteById(id);
 		
 	}
 
 	@Override
 	public Operateur updateOperateur(Operateur o) {
-		log.info("In method updateOperateur");
 		operateurRepository.save(o);
 		return o;
 	}
 
 	@Override
 	public Operateur retrieveOperateur(Long id) {
-		log.info("In method retrieveOperateur");
-		Operateur op =  operateurRepository.findById(id).orElse(null);
-		log.info(" Operateur: " + op);
-		return  op;
+		return operateurRepository.findById(id).orElse(null);
 	}
 
 	@Override
