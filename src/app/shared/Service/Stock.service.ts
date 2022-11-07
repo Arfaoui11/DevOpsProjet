47564e74c7408a39f207f7c15027381e5d6a1d97
DrissiOmar <omar.drissi@esprit.tn>
2022-11-07 20:29:33 +0100

    ip changee

--- src/app/shared/Service/Stock.service.ts
+++ src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'http://192.168.1.27:8089/SpringMVC/stock';
+  readonly API_URL = 'http://192.168.1.15:8089/SpringMVC/stock';
 
   constructor(private httpClient: HttpClient) { }
 
