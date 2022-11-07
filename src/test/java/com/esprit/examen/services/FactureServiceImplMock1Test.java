f1d4f0c32a86d0cc1234a4e45d0cc22563054641
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-11-08 00:00:45 +0100

    new update mahdi arfaoui

--- src/test/java/com/esprit/examen/services/FactureServiceImplMock1Test.java
+++ src/test/java/com/esprit/examen/services/FactureServiceImplMock1Test.java
@@ -1,4 +1,4 @@
-package com.esprit.examen.services;
+/*package com.esprit.examen.services;
 
 import com.esprit.examen.entities.Facture;
 import com.esprit.examen.repositories.FactureRepository;
@@ -101,4 +101,4 @@ public class FactureServiceImplMock1Test {
     }
 
 
-}
+}*/
