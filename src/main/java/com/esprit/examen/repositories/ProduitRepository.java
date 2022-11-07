f39b24bc7157698b4e02738a3fadfd82f6b7e69e
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-11-07 22:31:18 +0100

    new update mahdi arfaoui

---
+++ src/main/java/com/esprit/examen/repositories/ProduitRepository.java
@@ -0,0 +1,21 @@
+package com.esprit.examen.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.esprit.examen.entities.Produit;
+
+@Repository
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
